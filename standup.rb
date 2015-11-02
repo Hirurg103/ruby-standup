82cc44fb2fc12e558de9390d8524ba44eaae5367
Dzmitry Kovalenok <dzm.kov@gmail.com>
2015-11-02 09:05:25 +0300

    Rename generate -> generate_for

--- standup.rb
+++ standup.rb
@@ -5,14 +5,14 @@ $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require 'standup_message'
 
 class Standup < Thor
-  desc "generate" ,"generates my standup based on my yesterday's commits"
-  def generate
-    puts standup_message
+  desc "generate_for repo_url" ,"generates my standup based on my yesterday's commits"
+  def generate_for(repo_url)
+    puts standup_message_for(repo_url)
   end
 
   no_commands do
-    def standup_message
-      StandupMessage.generate
+    def standup_message_for(repo_url)
+      StandupMessage.for(repo_url)
     end
   end
 end
