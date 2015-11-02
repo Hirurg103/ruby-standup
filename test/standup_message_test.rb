7f3bc679b69a1cbb96d4e4c0610f2872dad6f26c
Dzmitry Kovalenok <dzm.kov@gmail.com>
2015-11-02 09:50:29 +0300

    Refactor standup message test a bit

--- test/standup_message_test.rb
+++ test/standup_message_test.rb
@@ -1,21 +1,32 @@
 require_relative './test_helper'
 
-describe StandupMessage do
-  describe 'when I made only commit yesterday' do
-    repo_url = File.expand_path('../fixtures/with_1_commit_yesterday', __FILE__)
-    FileUtils.rm_rf repo_url
-    FileUtils.mkdir_p repo_url
-    repo = Rugged::Repository.init_at(repo_url)
+def create_commit(attrs)
+  Rugged::Commit.create(@repo, {
+    message: 'Initial commit',
+    parents: @repo.empty? ? [] : [@repo.head.target].compact,
+    update_ref: 'HEAD',
+    tree: @repo.index.write_tree(@repo)
+  }.merge(attrs))
+end
 
-    Rugged::Commit.create(repo, {
-      message: 'Add public page for users',
-      parents: repo.empty? ? [] : [repo.head.target].compact,
-      update_ref: 'HEAD',
-      tree: repo.index.write_tree(repo)
-    })
+describe StandupMessage do
+  before do
+    @repo_url = File.expand_path('../fixtures/with_1_commit_yesterday', __FILE__)
+    FileUtils.mkdir_p @repo_url
+    @repo = Rugged::Repository.init_at(@repo_url)
+  end
+
+  after do
+    FileUtils.rm_rf @repo_url
+  end
+
+  describe 'when I made only commit yesterday' do
+    before do
+      create_commit message: 'Add public page for users'
+    end
 
     it 'should include this commit in my standup' do
-      StandupMessage.for(repo_url).must_equal 'Yesterday I add public page for users'
+      StandupMessage.for(@repo_url).must_equal 'Yesterday I add public page for users'
     end
   end
 end
